f33b5967f3f87088142ceb163b8d6b41c0b702f6
bharaths002 <mailmehere1820@gmail.com>
2025-06-26 16:32:46 +0530

    Set DEBUG=True for Render troubleshooting

--- company/company/settings.py
+++ company/company/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-for-local-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
